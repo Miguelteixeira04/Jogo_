13d9e3c7a78de79f5d4dc75936b0b717aec7bc43
Diogoscabral <diogocabral880@gmail.com>
2023-05-08 18:33:58 +0100

    Update Jogo.py

--- Jogo.py
+++ Jogo.py
@@ -1,4 +1,5 @@
 #Menu
 def menu():
-    print("Bem-vido ao jogo do semáforo: ")
-    
+    print("Bem-vido ao jogo do semáforo!!!!")
+    nome1 = input("Insira o nome do jogador 1: ")
+    nome2 = input("Insira o nome do jogador 2: ")
