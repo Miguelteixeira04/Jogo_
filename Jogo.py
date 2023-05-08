88885f0d58521a64d9d417fbf145b5a9ab62418e
Diogoscabral <diogocabral880@gmail.com>
2023-05-08 23:09:22 +0100

    Update Jogo.py

--- Jogo.py
+++ Jogo.py
@@ -6,6 +6,8 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
 
+matriz = [["","","",""], ["","","",""], ["","","",""]]
+
 #Menu
 def menu():
 
@@ -14,7 +16,7 @@ def menu():
     print("Jogar uma partida (1)")
     print("Carregar uma partida apartir de um ficheiro (2)")
     print("Apresentar uma descrição do jogo (3)")
-    print("Sair da aplicação (4)")
+    print("Sair da aplicação (4)\n")
     
     menu = int(input(""))
     
@@ -22,22 +24,23 @@ def menu():
         jogar()
     
 def jogar():
-    print("Jogar 1v1 (1)")
-    print("Jogar contra um bot (2)")    
+    print("\nJogar 1v1 (1)")
+    print("Jogar contra um bot (2)\n")    
     
     jogar1 = int(input(""))
     
     if jogar1 == 1:
-        nome1 = input("Insira o nome do jogador 1: ")
+        nome1 = input("\nInsira o nome do jogador 1: ")
         nome2 = input("Insira o nome do jogador 2: ")
         
         primeiro = rd.randint(1,2)
         
         if primeiro == 1:
-            print("O primeiro jogador é o " + nome1)
+            print("\nO primeiro jogador é o " + nome1)
             print("O segundo jogador é o " + nome2)
+            jogo()
         else:
-            print("O primeiro jogador é o " + nome2)
+            print("\nO primeiro jogador é o " + nome2)
             print("O segundo jogador é o " + nome1)
             jogo()
         
@@ -47,27 +50,61 @@ def jogar():
         primeiro = rd.randint(1,2)
         
         if primeiro == 1:
-            print("O primeiro jogador é o " + nome1)
+            print("\nO primeiro jogador é o " + nome1)
             print("O segundo jogador é o BOT\n")
             print("\n")
+            jogo()
         else:
-            print("O primeiro jogador é o BOT")
+            print("\nO primeiro jogador é o BOT")
             print(f"O segundo jogador é o {nome1}")    
-            print("\n")    
+            print("\n")   
+            jogo() 
     else: 
         print("Escolha o numero 1 ou 2.\n")
         jogar()
         
-def jogo():
-    matriz = [["","","",""], ["","","",""], ["","","",""]]
+def imprimir_matriz():
     
-    print("\n")    
-    print("0:\t" + " | " + matriz[0][0] + " | " + matriz[0][1] + " | " + matriz[0][2] + " | " + matriz[0][3])
-    print("----------------")
-    print("1:\t" + " | "+ matriz[1][0] + " | " + matriz[1][1] + " | " + matriz[1][2] + " | " + matriz[1][3])
-    print("----------------")
-    print("2:\t" + " | " + matriz[2][0] + " | " + matriz[2][1] + " | " + matriz[2][2] + " | " + matriz[2][3])
+    print("\n") 
+    print("\t   A    B    C    D")   
+    print("\t _____________________")
+    print("0:\t" + " |  " + str(matriz[0][0]) + "  |  " + str(matriz[0][1]) + "  |  " + str(matriz[0][2]) + "  |  " + str(matriz[0][3]) + "  |  " )
+    print("\t |----|----|----|----|")
+    print("1:\t" + " |  "+ str(matriz[1][0]) + "  |  " + str(matriz[1][1]) + "  |  " + str(matriz[1][2]) + "  |  " + str(matriz[1][3]) + "  |  " )
+    print("\t |----|----|----|----|")
+    print("2:\t" + " |__" + str(matriz[2][0]) + "__|__" + str(matriz[2][1]) + "__|__" + str(matriz[2][2]) + "__|__" + str(matriz[2][3]) + "__|" )
     
 
+def jogo():
+    
+    imprimir_matriz()    
+    colocar()
+
+def colocar():
+    
+    l = int(input("\nLinha: "))
+    c = int(input("Coluna: "))
+    
+    print("\nGreen [G]\nYellow [Y]\nRed [R]")
+    cor = input("").upper()
+    
+    if matriz[l][c] == "" or matriz[l][c] == None:
+        if cor == "G":
+            matriz[l][c] = "G"
+        elif cor == "Y":
+            matriz[l][c] = "Y"
+        else:
+            matriz[l][c] = "R"
+    else:
+        print("Posição já preenchida.")
+        colocar()
+        
+def botjoga():
+    l = rd.rdint(0,3)
+    c = rd.rdint(0,4)
+    while matriz[l][c] == "":
+        l = rd.rdint(0,3)
+        c = rd.rdint(0,4)
+        
 menu()
 
