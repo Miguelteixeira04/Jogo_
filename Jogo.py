f3aa5109211d6343cb8273afff3b05828b270d1d
Diogoscabral <diogocabral880@gmail.com>
2023-05-10 20:27:03 +0100

    Update Jogo.py

--- Jogo.py
+++ Jogo.py
@@ -6,11 +6,11 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
 
+jogador_atual = " "
 matriz = [[" "," "," "," "], [" "," "," "," "], [" "," "," "," "]]
 
 #Menu
 def menu():
-
 
     print("|---------------------------------------------------|")
     print("|\tBem-vindo ao jogo matemático do semáforo    |")
@@ -20,15 +20,15 @@ def menu():
     print("Apresentar uma descrição do jogo (3)")
     print("Sair da aplicação (4)\n")
 
-    print("                                                                           _________________________________")
+    print("                                                                           ---------------------------------")
     print("                                                                          |Trabalho realizado por:          | ")
     print("                                                                          |                                 |")
     print("                                                                          |   Diogo Cabral         al78834  |")
     print("                                                                          |   Maria Inês Cardoso   al78222  |")
     print("                                                                          |   Miguel Teixeira      al78321  |")
     print("                                                                          |                                 | ")
-    print("                                                                          |---------------------------------|")
-
+    print("                                                                          |                                 |")
+    print("                                                                           ---------------------------------")
     
     menu = int(input(""))
     
@@ -52,13 +52,14 @@ def jogar():
             print("O segundo jogador é o " + nome2)
             primeiro = nome1
             segundo = nome2
-            jogo()
+            jogo(primeiro, segundo, jogar1)
         else:
             print("\nO primeiro jogador é o " + nome2)
             print("O segundo jogador é o " + nome1)
             primeiro = nome2
             segundo = nome1
-            jogo()
+            jogo(primeiro, segundo, jogar1)
+     
         
     elif jogar1 == 2:
         nome1 = input("Insira o nome do jogador: ")
@@ -69,26 +70,22 @@ def jogar():
             print("\nO primeiro jogador é o " + nome1)
             print("O segundo jogador é o BOT\n")
             print("\n")
-            jogo()
+            jogo(nome1, "BOT", jogar1)
         else:
             print("\nO primeiro jogador é o BOT")
             print(f"O segundo jogador é o {nome1}")    
             print("\n")   
-            jogo() 
+            jogo("BOT", nome1, jogar1) 
     else: 
         print("Escolha o numero 1 ou 2.\n")
         jogar()
+
         
 def imprimir_matriz():
-    
-    #A = 0
-    #B = 1
-    #C = 2
-    #D = 3
 
     print("\n") 
     print("\t   A    B    C    D")   
-    print("\t _________________________")
+    print("\t  _______________________")
     print("0:\t" + " |  " + str(matriz[0][0]) + "  |  " + str(matriz[0][1]) + "  |  " + str(matriz[0][2]) + "  |  " + str(matriz[0][3]) + "  |  " )
     print("\t |-----|-----|-----|-----|")
     print("1:\t" + " |  "+ str(matriz[1][0]) + "  |  " + str(matriz[1][1]) + "  |  " + str(matriz[1][2]) + "  |  " + str(matriz[1][3]) + "  |  " )
@@ -96,32 +93,36 @@ def imprimir_matriz():
     print("2:\t" + " |__" + str(matriz[2][0]) + "__|__" + str(matriz[2][1]) + "__|__" + str(matriz[2][2]) + "__|__" + str(matriz[2][3]) + "__|" )
     
 
-def jogo():
+def jogo(primeiro, segundo, jogar1):
     
-    while True:
-     imprimir_matriz()
-     colocar()
-     
-     victory, winner = verificar_vitoria()
-     if victory:
-            print(f"\n{winner} wins!")
-            break
-   
-   
-def colocar():
-    
+    jogador_atual = primeiro
+    vitoria = False
 
+    if jogar1 == 1:
+       
+        while True:
+         imprimir_matriz()
+         jogador_atual, vitoria = colocar(primeiro, segundo, jogador_atual)
+         if vitoria == True:
+             break
+          
+    elif jogar1 == 2:
+        while True:
+            imprimir_matriz()
+            jogada_bot()
+            
+            
+        
+   
+   
+def colocar(primeiro, segundo, jogador_atual):
+    
     l = int(input("\nLinha: "))
     c = int(input("Coluna: "))
     
-    #A = 0
-    #B = 1
-    #C = 2
-    #D = 3
-    
     if l < 0 or l > 2 or c < 0 or c > 3:
         print("Posição inválida.")
-        colocar()
+        colocar(primeiro, segundo, jogador_atual)
         return
 
     print("\nGreen [G]\nYellow [Y]\nRed [R]")
@@ -144,26 +145,57 @@ def colocar():
         
     if cor == "Y" and matriz[l][c] == "G":
         matriz[l][c] = cor   
+        colocar(primeiro, segundo, jogador_atual) 
+        
+    vitoria, cor = verificar_vitoria(jogador_atual)
+    if vitoria:
+        print(f"\nO jogador {cor} venceu!")
+    elif jogador_atual == primeiro:
+        print(f"\nTurno do(a) {segundo}")
+        jogador_atual = segundo
+    elif jogador_atual == segundo:
+        print(f"\nTurno do(a) {primeiro}")
+        jogador_atual = primeiro
+    
+    return jogador_atual, vitoria
         
     
-        colocar() 
         
-def verificar_vitoria():
-    for c in range(4): 
-        if matriz[0][c] == matriz[1][c] == matriz[2][c] != ' ':  
-            imprimir_matriz()
-            return True, matriz[0][c] 
-    for l in range(3):
-        if matriz[l][0] == matriz[l][1] == matriz[l][2] != ' ':
-            imprimir_matriz()
-            return True, matriz[l][0]
-    return False, None  
+        
+        
+def verificar_vitoria(jogador_atual):
+    for i in range(3):
+        if matriz[i][0] == matriz[i][1] == matriz[i][2] != ' ':
+            return True, jogador_atual
 
-def jogada_bot(matriz):
+    for j in range(3):
+        if matriz[0][j] == matriz[1][j] == matriz[2][j] != ' ':
+            return True, jogador_atual
+
+    if matriz[0][0] == matriz[1][1] == matriz[2][2] != ' ':
+        return True, jogador_atual
+    
+    if matriz[0][1] == matriz[1][2] == matriz[2][3] != ' ':
+        return True, jogador_atual
+    
+    if matriz[0][3] == matriz[1][2] == matriz[2][1] != ' ':
+        return True, jogador_atual
+    
+    if matriz[0][2] == matriz[1][1] == matriz[2][0] != ' ':
+        return True, jogador_atual
+
+    if matriz[0][2] == matriz[1][1] == matriz[2][0] != ' ':
+        return True, jogador_atual
+
+    return False, None
+
+def jogada_bot():
     while True:
         l = rd.randint(0, 2)
         c = rd.randint(0, 3)
+        
         cor = ['G', 'Y', 'R']
+        
         for i in range(3):
             if matriz[l][c] == " ":
                 if cor[i] == "G" and matriz[l][c] == " ":
